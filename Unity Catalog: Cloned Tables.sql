da3627c86fcf5f30939f6e28552156972a5e59bc
amlucius <amlucius>
2023-12-06 00:03:36 +0000

    updates to syntax

--- Unity Catalog: Cloned Tables.sql
+++ Unity Catalog: Cloned Tables.sql
@@ -5,8 +5,20 @@ desc extended main.default.diamonds
 -- COMMAND ----------
 
 -- DBTITLE 1,External table
-desc table extended main.default.wine
+desc extended main.default.wine
 
 -- COMMAND ----------
 
 CREATE TABLE main.default.wine_clone SHALLOW CLONE main.default.wine location 's3://alucius-sandbox-group-b/wine_clone'
+
+-- COMMAND ----------
+
+desc extended main.default.wine_clone
+
+-- COMMAND ----------
+
+CREATE EXTERNAL TABLE main.default.wine_clone_ext SHALLOW CLONE main.default.wine location 's3://alucius-sandbox-group-b/wine_clone_ext'
+
+-- COMMAND ----------
+
+desc extended main.default.wine_clone_ext
